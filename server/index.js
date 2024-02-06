4ea5ea4bcaf9bbe8a7171dae899e9fd28ef06be6
Parvez Khan <137421089+Parvezkhan0@users.noreply.github.com>
2024-02-06 10:23:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,12 +19,10 @@ io.on('connect', (socket) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if(error) return callback(error);
-
     socket.join(user.room);
 
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     callback();
